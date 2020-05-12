dddb7e94a0d0b3a73ae8d74ff7a28036c405d3a0
julien pichonnat <julien.pichonnat@outlook.com>
2020-05-12 16:31:38 +0200

    creation de test unitaire

---
+++ ExIntegrationContinue/UnitTestExIntegrationContinue/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExIntegrationContinue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExIntegrationContinue")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18b8228e-95e0-4168-83c3-c8e1c05c3669")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExIntegrationContinue/UnitTestExIntegrationContinue/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ExIntegrationContinue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestExIntegrationContinue
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var calc = new ConverterLibrary();
+
+            //Act
+            var result = calc.GetInt("42");
+
+            //Assert
+            Assert.AreEqual(42, result);
+        }
+    }
+}
